4d6835fab89504c52c08968ed3e91d6b222ecfde
bavayra <x.bavayra@gmail.com>
2025-07-04 18:22:59 +0200

    more tasks with a roll dice

--- index.js
+++ index.js
@@ -11,25 +11,48 @@ document.getElementById("p3").textContent = student;
 
 let username;
 username = prompt("What's your name?");
-console.log(username);*/
+console.log(username); ITS THE 1ST ONE */
 
 /*let username;
 
 document.getElementById("mySubmit").onclick = function(){
   username = document.getElementById("myText").value;
   document.getElementById("myH1").textContent = `Hello ${username}`
-}*/
+}ITS THE 2ND ONE*/
 
-const PI = 3.14159;
+/*const PI = 3.14159;
 let radius;
 let circumference;
-
-
 
 document.getElementById("mySubmit").onclick = function(){
   radius = document.getElementById("myText").value; 
   radius = Number(radius);
   circumference = 2 * PI * radius;
   document.getElementById("myH3").textContent = circumference;
+} ITS THE 3RD ONE*/
 
-}
+/*const min = 50;
+const max = 100;
+
+let randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+console.log(randomNum); THE 4TH ONE */
+
+/*const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function(){
+  randomNum1 = Math.floor(Math.random() * max) + min;
+  randomNum2 = Math.floor(Math.random() * max) + min;
+  randomNum3 = Math.floor(Math.random() * max) + min;
+  label1.textContent = randomNum1;
+  label2.textContent = randomNum2;
+  label3.textContent = randomNum3;
+} THE 4.5TH ONE */
