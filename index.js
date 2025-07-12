93ac3836213a1df811a29dfa8dc528af15c3b9bb
bavayra <x.bavayra@gmail.com>
2025-07-12 19:29:57 +0200

    another task with a guess the number game

--- index.js
+++ index.js
@@ -144,3 +144,38 @@ mySubmit.onclick = function(){
       console.log("Invalid credentials!");
     }
   } THE 8TH ONE*/
+
+ /* const minNum = 1;
+  const maxNum = 100;
+
+  const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+  let attempts = 0;
+  let guess;
+  let running = true;
+
+  while(running){
+
+    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}`);
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+      window.alert("Please enter a valid number");
+    }
+    else if(guess < minNum || guess > maxNum){
+      window.alert("Please enter a valid number");
+    }
+    else{
+      attempts++;
+      if(guess < answer){
+        window.alert("Too low");
+      }
+      else if(guess >answer){
+        window.alert("too high");
+      }
+      else{
+        window.alert(`congrats, the answer ${answer} is correct. It took you ${attempts} turns to win`);
+         running = false;
+      }
+    }
+  } TTHE 9TH ONE WITH GUESSING GAME */
