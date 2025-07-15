d1ddde27f5226c7d9d897f0622b3574694398ad7
bavayra <x.bavayra@gmail.com>
2025-07-15 18:15:14 +0200

    created a temperature converter

--- index.js
+++ index.js
@@ -179,3 +179,25 @@ mySubmit.onclick = function(){
       }
     }
   } TTHE 9TH ONE WITH GUESSING GAME */
+
+  const textBox = document.getElementById("textBox");
+  const toFahrenheit = document.getElementById("toFahrenheit");
+  const toCelsius = document.getElementById("toCelsius");
+  const result = document.getElementById("result");
+  let temp;
+
+  function convert(){
+    if(toFahrenheit.checked){
+      temp = Number(textBox.value);
+      temp = temp * 9 / 5 + 32;
+      result.textContent = temp.toFixed(1) + "F";
+    }
+    else if(toCelsius.checked){
+      temp = Number(textBox.value);
+      temp = (temp - 32) * 5 / 9;
+      result.textContent = temp.toFixed(1) + "C";
+    }
+    else{
+      result.textContent = "Select a unit";
+    }
+  }
