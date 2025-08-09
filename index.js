6e4474a60259954b28068c5388b0504dddfb68a7
bavayra <x.bavayra@gmail.com>
2025-08-09 18:43:12 +0200

    fetch, async/await and pokemons

--- index.js
+++ index.js
@@ -819,3 +819,38 @@ async function doChores(){
        // .then(value => {console.log(value); console.log("You finished all the chores")})
       //  .catch(error => console.error(error)); 
   doChores()  THE 25 WITH AWAIT/ASYNC */
+
+  //fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    //.then(response => {
+
+      //if(!response.ok){
+        //throw new Error("Coldnt fetch resourse");
+      //}
+      //return response.json();
+    //})
+    //.then(data => console.log(data.name))
+    //.catch(error => console.error(error)); //this is a fetch method, see await-asynq below
+
+    async function fetchData(){
+
+      try{
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok){
+          throw new Error("Couldnt fetch");
+        }
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+      }
+
+      catch(error){
+        console.error(error);
+      }
+    }
+
